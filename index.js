c394e8d46b9e8b8c4cdd49e9a0af905e4e6856aa
Michael A Nowiszewski <micski@gmail.com>
2017-12-04 23:21:27 +0000

    add onclick function fire

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 /*global $*/
+$(document).ready(function() {
+		function getQuote() {
+			$.ajax({
+				method: "GET",
+				url: 'https://got-quotes.herokuapp.com/quotes',
+				success: function(response) {
+					$('#quote').text(response.quote);
+					//   document.getElementById('quote').innerHTML = response.quote;
+					$('#character').text(response.character);
+					//   document.getElementById('character').innerHTML = response.character
+				}
+			});
+		}
+		getQuote();
+		$('#getQuote').on('click', function(e) {
+			getQuote();
+		});
+	})
+	
